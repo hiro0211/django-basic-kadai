42d1fc7e847213da7b1a01f018af09591a10fd32
hiro0211 <143386971+hiro0211@users.noreply.github.com>
2023-10-11 21:56:09 +0900

    Add files via upload

---
+++ kadai_010/crud/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Product
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+
+class TopView(TemplateView):
+    template_name = "top.html"
+
+class ProductListView(ListView):
+  model = Product
+  paginate_by = 3
+ 
+class ProductCreateView(CreateView):
+  model = Product
+  fields = '__all__'
+
+class ProductUpadateView(UpdateView):
+   model = Product
+   fields = "__all__"
+   template_name_suffix = "_update_form"
+
+class ProductDeleteView(DeleteView):
+   model = Product
+   success_url = reverse_lazy("list")
+
+class ProductDetailView(DetailView):
+   model = Product
+   context_object_name = 'Product'
+   template_name = "product_detail.html"
